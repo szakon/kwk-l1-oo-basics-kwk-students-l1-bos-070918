2b93c96a63b4033a447e086db73a978f6cf6e88a
szakon <sarahzakon@gmail.com>
2018-07-13 14:58:12 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,15 +7,15 @@ class Shoe
 
  def initialize(brand) # model, type, price)
    @brand = brand
-
    #@type = type
    #@price = price
  end
 end
+color = red
 
 shoes1=Shoe.new("Adidas")#, "red")  # "airmax", "sneakers", 100, )
 puts shoes1.brand
-puts shoes1.color = "red"
+puts shoes1.color
 
 #puts shoes1.type
 #puts shoes1.price
