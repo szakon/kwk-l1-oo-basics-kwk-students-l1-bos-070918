80f7c50d57539d0a7f33238ebe0bfa6131122599
szakon <sarahzakon@gmail.com>
2018-07-13 14:55:33 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
 # Make your shoe class here!
+
+class Shoe
+  attr_reader :brand,#, #:material
+  attr_accessor :
+
+
+ def initialize(brand) # model, type, price)
+   @brand = brand
+
+   #@type = type
+   #@price = price
+ end
+end
+
+shoes1=Shoe.new("Adidas")#, "red")  # "airmax", "sneakers", 100, )
+puts shoes1.brand
+puts shoes1.color
+
+#puts shoes1.type
+#puts shoes1.price
+
+
